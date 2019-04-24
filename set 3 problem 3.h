1a8a2fc9562e6fdcc5366ef13ab1c13eaeb182f8
sihuintaperez <38123330+sihuintaperez@users.noreply.github.com>
2019-04-24 08:15:34 -0500

    Add files via upload

---
+++ set 3 problem 3.h
@@ -0,0 +1,10 @@
+class point {
+    int x,y;
+public :
+    Point (int xx =0, int yy =0) {x = xx; y = yy ;}
+    int getX () const { return x;}
+    int getY () const { return y;}
+    void setX ( const int xx) {x = xx ;}
+    void setY ( const int yy) {y = yy ;}
+
+};
